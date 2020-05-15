c3352bbb1bb2cff64f633a9e870c3925cbd73160
Lightshadow244 <richi.lightshadow@gmail.com>
2020-05-15 22:49:36 +0200

    trade

--- CollectingDove/website/management/commands/trade.py
+++ CollectingDove/website/management/commands/trade.py
@@ -181,7 +181,7 @@ def TradeEurBtcTest(self, lastTrade, rate, compareDeltaRate, mode):
                 else:
                     pass
             else:
-                print('btc to eur was not traded')
+                #print('btc to eur was not traded')
         else:
             if(deltaRate > 0 and abs(deltaRate) > compareDeltaRate):
                 if(mode == 0):
@@ -201,7 +201,7 @@ def TradeEurBtcTest(self, lastTrade, rate, compareDeltaRate, mode):
                 else:
                     pass
             else:
-                print('eur to btc was not traded')
+                #print('eur to btc was not traded')
     else:
         print('No last Value in DB')
     return(r)
