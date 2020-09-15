6983ed7ac92db0b6e3bdb52779c5484b322ee4aa
Lightshadow244 <richi.lightshadow@gmail.com>
2020-09-15 15:56:07 +0200

    start backend graph

--- CollectingDove/website/views.py
+++ CollectingDove/website/views.py
@@ -1,11 +1,35 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from website.models import Trade_BTC_test, StopTrade
+from website.models import Trade_BTC_test, StopTrade, Trade_BTC
+from django.template import loader
 import csv
+from datetime import datetime, timezone, timedelta
 
 
 def index(request):
-    return render(request, 'CollectingDove/index.html')
+    template = loader.get_template('CollectingDove/index.html')
+
+    #letzten zwei Tage 12 DP in einer Stunde * 48 Stunden = 576 Datenpunkte
+    last2d = datetime.now() - timedelta(days=2)
+    last2d = last2d.replace(tzinfo=timezone.utc)
+    rates_2d = Trade_BTC.objects.filter(time__gte=last2d).order_by('time')
+    trades_2d = Trade_BTC.objects.filter(time__gte=last2d).exclude(btc = None, eur = None).order_by('time')
+
+    label = []
+    t = 0
+    for rate in rates_2d:
+        if(rate.time.hour > t):
+            t = rate.time.hour
+            label.append(t)
+        else:
+            label.append(None)
+    print(label);
+
+
+
+    context = {
+    }
+    return HttpResponse(template.render(context, request))
 
 def exportValue(request):
     response = HttpResponse(content_type='text/csv')
